ec4c7e609e537ba2da1296c5c3829d95c4b518b1
Carlos Gonçalves <carlos.goncalves@outsystems.com>
2021-04-01 18:10:57 +0100

    bot using only selenium :)

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import time
-import pyautogui
 from selenium import webdriver
 
 driver = webdriver.Chrome("./chromedriver")
@@ -8,13 +7,28 @@ driver.get("https://play.typeracer.com")
 time.sleep(10)
 
 pyautogui.hotkey('ctrl','alt','i')
-time.sleep(2)
+time.sleep(8)
 
 text = driver.find_element_by_class_name("gameView").text
 text = text.split("\n")
 
-print(text[-3])
-time.sleep(15)
-pyautogui.typewrite(text[-3], interval = 0.05)
+myInput = driver.find_element_by_class_name("txtInput")
+
+finalText = text[-3]
+
+n = 12 # chunk length
+chunks = [finalText[i:i+n] for i in range(0, len(finalText), n)]
+
+print("Chunked String", chunks)
+print("Going to sleep")
+time.sleep(10)
+print("Finished")
+
+for elem in finalText:
+    myInput.send_keys(elem)
+    time.sleep(0.01)
+
+print("Going to sleep")
 time.sleep(20)
+print("Finished")
 driver.quit()
