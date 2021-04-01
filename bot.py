91247314386fab4022ce0fbb770c4fad191bf998
Carlos Gonçalves <carlos.goncalves@outsystems.com>
2021-04-01 18:28:56 +0100

    remove all external dependencies besides selenium

--- bot.py
+++ bot.py
@@ -1,34 +1,40 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome("./chromedriver")
+def play():
+    driver = webdriver.Chrome("./chromedriver")
+    #Opens typeracer
+    driver.get("https://play.typeracer.com")
+    time.sleep(5)
+    
+    #Entering a race
+    root = driver.find_element_by_tag_name("html")
+    root.send_keys(Keys.CONTROL,Keys.ALT,'i')
 
-driver.get("https://play.typeracer.com")
-time.sleep(10)
+    # Finds the text
+    time.sleep(3)
+    text = driver.find_element_by_class_name("gameView").text
+    text = text.split("\n")
+    finalText = text[-3]
+    
+    #Injects the text on the input char by char
+    time.sleep(10)
+    myInput = driver.find_element_by_class_name("txtInput")
+    for elem in finalText:
+        myInput.send_keys(elem)
+        time.sleep(0.01)
+    
+    # Waits a bit so you can contemplate the winning message :D
+    time.sleep(20)
+    print("Finished")
+    driver.quit() 
 
-pyautogui.hotkey('ctrl','alt','i')
-time.sleep(8)
+if __name__ == "__main__":
+    play()
 
-text = driver.find_element_by_class_name("gameView").text
-text = text.split("\n")
 
-myInput = driver.find_element_by_class_name("txtInput")
-
-finalText = text[-3]
-
-n = 12 # chunk length
-chunks = [finalText[i:i+n] for i in range(0, len(finalText), n)]
-
-print("Chunked String", chunks)
-print("Going to sleep")
-time.sleep(10)
-print("Finished")
-
-for elem in finalText:
-    myInput.send_keys(elem)
-    time.sleep(0.01)
-
-print("Going to sleep")
-time.sleep(20)
-print("Finished")
-driver.quit()
+    
+    
+    
+    
